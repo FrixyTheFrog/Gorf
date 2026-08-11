4abcc73acfe072582eb83d21809df70cfecf21e5
FrixyTheFrog <114127309+FrixyTheFrog@users.noreply.github.com>
2026-08-12 00:54:22 +1000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "floating around",     // this is what people actually see
-        type: 4,               // Custom
+        state: "the stars",     // this is what people actually see
+        type: 3,               // Custom
       },
     ],
   },
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#7166D1",
+      secondary: "#6671D1",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
